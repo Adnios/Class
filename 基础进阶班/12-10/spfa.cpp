995077614e235f4dbd94e0cbafc23ad9601084a6
Adnois <2780199647@qq.com>
2019-12-14 10:44:38 +0800

    test

---
+++ 基础进阶班/12-10/spfa.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int maxx=3200000;
+const int inf=0x3f3f3f3f;
+struct edge{
+    int next,to,dis;
+}edge[maxx];
+int n,m,cnt,head[maxx],dis[maxx],vis[maxx];
+void add(int from,int to,int dis){
+    edge[++cnt].next=head[from];
+    edge[cnt].to=to;
+    edge[cnt].dis=dis;
+    head[from]=cnt;
+}
+void spfa(){
+    memset(dis,inf,sizeof(dis));
+    vis[1]=1;
+    dis[1]=0;
+    queue<int> Q;
+    Q.push(1);
+    while(!Q.empty()){
+        int u=Q.front();
+        Q.pop();
+        vis[u]=0;
+        for(int i=head[u];i!=0;i=edge[i].next){
+            int to=edge[i].to;
+            int di=edge[i].dis;
+            if(dis[to]>dis[u]+di){
+                dis[to]=dis[u]+di;
+                if(!vis[to]){
+                    vis[to]=1;
+                    Q.push(to);
+                }
+            }
+        }
+    }
+}
+int main(){
+    cin>>n>>m;
+    for(int i=1;i<=m;i++){
+        int x,y,v;
+        cin>>x>>y>>v;
+        add(x,y,v);
+        add(y,x,v);
+    }
+    spfa();
+    cout<<dis[n]<<endl;
+}
---
+++ 基础进阶班/12-10/邻接表.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+const int maxx=100001;
+struct Edge{
+    int next,to,dis;
+}edge[maxx];
+int head[maxx],cnt,n,m;
+void add_edge(int from,int to,int dis){
+    edge[++cnt].next=head[from];
+    edge[cnt].to=to;
+    edge[cnt].dis=dis;
+    head[from]=cnt;
+}
+
+int main()
+{
+    cin>>n>>m;
+    for (int i = 0;i<m;i++) {
+        int a, b, c;
+        cin>>a>>b>>c;
+        add_edge(a, b, c);
+    }
+    for(int i=1;i<=n;i++){
+        cout<<i<<"  ";
+        for(int j=head[i];j!=0;j=edge[j].next){
+            cout<<edge[j].to<<" ";
+        }
+        cout<<endl;
+    }
+}
