1f1cb2f01d14fb6c3cab8dc6e9a9d26db9761135
Adnios <2780199647@qq.com>
2020-05-09 16:11:56 +0800

    git.sh

--- test.cpp
+++ test.cpp
@@ -1,9 +1,69 @@
-#include<iostream>
-#include<algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+struct edge{
+	int u,v,w;
+}e[2505];
+long long n,m,k,f[105][105]; 
+struct mat{
+	long long a[105][105];
+	mat(int x=63){//¹¹Ôìº¯Êý 
+		memset(a,x,sizeof(a));
+	}
+	mat operator*(const mat& b)const{
+		mat ans;//ans=a*b
+		for(int k=1;k<=n;k++){
+			for(int i=1;i<=n;i++){
+				for(int j=1;j<=n;j++){
+					ans.a[i][j]=min(ans.a[i][j],a[i][k]+b.a[k][j]);
+				} 
+			}
+		}
+		return ans;
+	}
+}a;
+mat fpow(mat x,int y){
+	mat ans;
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=n;j++){
+			ans.a[i][j]=f[i][j];//k=0
+		}
+	}
+	while(y>0){
+		if(y&1)
+			ans=ans*x;
+		x=x*x;
+		y>>=1;
+	}
+	return ans;
+}
+
 int main(){
-	int a[10]={1,2,2,3,3,3,5,5,5,6};
-	//å¯»æ‰¾3
-	cout<<lower_bound(a,a+10,9)-a<<","<<upper_bound(a,a+11,9)-a<<endl;
-	return 0;
-} 
+	memset(f,63,sizeof(f));
+	cin>>n>>m>>k;
+	for(int i=1;i<=n;i++)
+		f[i][i]=0;
+	for(int i=1;i<=m;i++){
+		cin>>e[i].u>>e[i].v>>e[i].w;
+		f[e[i].u][e[i].v]=e[i].w;
+	}
+	for(int k=1;k<=n;k++){
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=n;j++){
+				f[i][j]=min(f[i][j],f[i][k]+f[k][j]);
+			}
+		}
+	}
+	for(int k=1;k<=m;k++){
+		int u=e[k].u,v=e[k].v,w=e[k].w;
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=n;j++){
+				a.a[i][j]=min(a.a[i][j],min(f[i][j],f[i][u]+f[v][j]-w));
+			}
+		}
+	}
+	if(k==0)
+		cout<<f[1][n]<<endl;
+	else
+		cout<<fpow(a,k).a[1][n]<<endl; 
+}
+
