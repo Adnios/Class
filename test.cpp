af6476c6cc658b760291f0ca1851ccf605c5fc38
Adnios <2780199647@qq.com>
2020-05-13 13:36:11 +0800

    git.sh

--- test.cpp
+++ test.cpp
@@ -1,12 +1,8 @@
-#include <iostream>
-#include <vector>
-#include <cstdio>
-#define mod 100007
+#include <bits/stdc++.h>
 using namespace std;
-
-vector<int> Hash[100007];
-long long maxx, t, n, num;
-
+const int mod=10005;
+long long t,n,num,maxx;
+vector<int> Hash[10005];
 int read(){
     int w=1,q=0;
 	char ch=' ';
@@ -15,35 +11,30 @@ int read(){
     while(ch>='0'&&ch<='9')q=q*10+ch-'0',ch=getchar();
     return w*q;
 }
-
-int max(int x, int y) {
-    return (x > y) ? x : y;
+bool check(int val){
+	long long pos=(val%mod + mod) %mod;
+	maxx=max(pos,maxx);
+	for(int k=0;k<Hash[pos].size();k++){
+		if(Hash[pos][k]==val)
+			return false;
+	}
+	Hash[pos].push_back(val);
+//	cout<<val<<endl;
+	return true;
 }
-
-bool check(int val) {
-    int pos = (val % mod + mod) % mod;
-    maxx = max(maxx, pos);
-    for (int k = 0; k < Hash[pos].size(); k++) {
-        if (Hash[pos][k] == val) {
-            return true;
-        }
-    }
-    Hash[pos].push_back(val);
-    return false;
-}
-
-int main() {
-    t=read();
-    while (t--) {
-        n=read();
-        for (int i = 0; i <= maxx; i++) Hash[i].clear();
-        maxx = 0;
-        for (int i = 1; i <= n; i++) {
-            num=read();
-            if(!check(num))
-                printf("%lld ",num);
-        }
-        cout << "\n";
-    }
-    return 0;
+int main(){
+	t=read();
+	while(t--){
+		n=read();
+		for(int i=0;i<=maxx;i++){
+			Hash[i].clear();
+		}
+		for(int i=1;i<=n;i++){
+			num=read();
+			if(check(num))
+				cout<<num<<" "; 
+		} 
+		cout<<endl; 
+	}
+	
 }
