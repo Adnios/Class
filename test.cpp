724ca7fed52ae7fbcb7622c3cceacaebe95b97aa
Adnios <2780199647@qq.com>
2020-06-06 23:11:58 +0800

    git.sh

--- test.cpp
+++ test.cpp
@@ -1,35 +1,77 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#define L 500005
 using namespace std;
-const int maxn=10005;
-int n;
-long long dp[maxn];//dp[j],子集之和为j时的方案数 
-//14(背包的体积)，（1（即使物品体积又是物品价值），2，3，4，5，6，7）
-int main(){
-	cin>>n;
-	int m=(1+n)*n/2;//20*39
-	if(m%2==1){
-		cout<<0;
-		return 0;
-	}
-	m=m/2;	
-	dp[0]=1;
-	for(int i=1;i<=n;i++){
-		for(int j=m;j>=i;j--){
-			dp[j]=dp[j-i]+dp[j];
+int Size,n;
+int flag=0; 
+int binary_Search(int *S,int x,int low ,int high)
+{
+    int mid = (low+high)/2;
+    if(x<S[0]) return 0;
+    else if(S[Size-1]<=x) return Size-1;
+//    cout<<low<<" "<<high<<endl;
+    if(low>=high) return -1;
+    else{
+        if(S[mid] == x) return mid;
+        else if(x>S[mid]&&x<S[mid+1]) {
+        	flag++;
+        	return mid;
 		}
-	}
-	cout<<dp[m]/2;
-} 
-for(int i=1;i<=m;i++){
-		for(int j=t;j>=v[i];j--){
-			dp[j]=max(dp[j-v[i]]+c[i],dp[j]);
-		}
-	}
-	
-for(int i=1;i<=m;i++){
-		for(int j=v[i];j<=t;j++){
-			dp[j]=max(dp[j],dp[j-v[i]]+c[i]);
-		}
-	}
+        else if(S[mid] < x) return binary_Search(S,x,mid,high);
+        else if(S[mid] > x) return binary_Search(S,x,low,mid);
+    }
+}
 
+void quick_sort(int *S,int low,int high)
+{
+    if(low<high)
+    {
+        int i = low;
+        int j = high;
+        int x = S[low];
+        while(i<j)
+        {
+            while(i<j && S[j] >= x) j--;
+            if(i<j) S[i++] = S[j];
+            while(i<j && S[i] <= x) i++;
+            if(i<j) S[j--] = S[i];
+        }
+        S[i] = x;
+        quick_sort(S,low,i-1);
+        quick_sort(S,i+1,high);
+    }
+}
+int main()
+{
+
+    int S[L];
+    scanf("%d%d",&Size,&n);
+    for(int i=0;i<Size;i++)
+        scanf("%d",&S[i]);
+    quick_sort(S,0,Size-1);
+    int l,r;
+    for(int i=0;i<n;i++)
+    {
+    	flag=0;
+        scanf("%d%d",&l,&r);
+        int num1 = binary_Search(S,l,0,Size-1);
+        int num2 = binary_Search(S,r,0,Size-1);
+//        cout<<num1<<" "<<num2<<" "<<flag<<endl; 
+        
+        if(flag==2||flag==1)
+        	printf("%d\n",num2-num1);
+        else
+        	printf("%d\n",num2-num1+1);
+    }
+
+    return 0;
+}
+/**
+5 3
+1 3 7 9 11
+4 6
+7 12 
+8 9
+**/ 
 
